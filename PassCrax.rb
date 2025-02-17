78fd506ce64439b0d73874929d3c521cf77b32fa
TAUREAN312 <jbotchwayowusu@gmail.com>
2025-02-17 20:39:46 +0000

    Update PassCrax.rb

--- PassCrax.rb
+++ PassCrax.rb
@@ -1,103 +1,28 @@
 #!/usr/bin/env ruby
-require 'digest'
-
+require './wordlist_cracker'
+require './pass_analyzer'
 begin
-   while true
-    grn = "\e[32m"
-    blu = "\e[34m"
-    ylw = "\e[33m"
-    red = "\e[31m"
-    rst = "\e[0m"
+  while true
+   grn = "\e[32m"
+   blu = "\e[34m"
+   ylw = "\e[33m"
+   red = "\e[31m"
+   rst = "\e[0m"
 
-   puts "\n#{blu}Enter Hash String:#{rst}"
-    hash = gets.chomp
-    puts "\n#{blu}Enter Wordlist Filename:#{rst}"
-    wordlist = gets.chomp
-
-   puts "\n#{blu}SELECT HASH FUNCTIONS\n#{rst} #{grn}[1] MD5\n [2] SHA-1\n [3] SHA-256\n [4] SHA-384\n [5] SHA-512\n#{rst}"
-   input = gets.chomp.to_i
-  def brute_md5(wordlist, hash)
-       File.readlines(wordlist).each do |word|
-           word.chomp!
-           return word if
-           Digest::MD5.hexdigest(word) == hash
-       end
-       nil
-   end
-
-   def brute_sha256(wordlist, hash)
-       File.readlines(wordlist).each do |word|
-           word.chomp!
-           return word if
-           Digest::SHA256.hexdigest(word) == hash
-       end
-       nil
-   end
-
-   def brute_sha1(wordlist, hash)
-       File.readlines(wordlist).each do |word|
-           word.chomp!
-           return word if
-           Digest::SHA1.hexdigest(word) == hash
-       end
-       nil
-   end
-
-   def brute_sha2(wordlist, hash)
-       File.readlines(wordlist).each do |word|
-           word.chomp!
-           return word if
-           Digest::SHA2.hexdigest(word) == hash
-       end
-       nil
-   end
-
-   def brute_sha384(wordlist, hash)
-       File.readlines(wordlist).each do |word|
-           word.chomp!
-           return word if
-           Digest::SHA384.hexdigest(word) == hash
-       end
-       nil
-   end
-
-   def brute_sha512(wordlist, hash)
-       File.readlines(wordlist).each do |word|
-           word.chomp!
-           return word if
-           Digest::SHA512.hexdigest(word) == hash
-       end
-       nil
-   end
-   case input
+   puts "\n#{blu}SElECT ONE\n#{rst}#{grn} [1]Password Analyzer\n [2]Password Cracker#{rst}"
+   choose = gets.chomp.to_i
+   case choose
    when 1
-    result = brute_md5(wordlist, hash)
-   puts result ?
-   "#{grn}Password Found:#{rst} #{ylw} #{result} #{rst}" : "#{red}Password Not Found!#{rst}"
+       pass_analyze()
 
    when 2
-    result = brute_sha1(wordlist, hash)
-   puts result ?
-   "#{grn}Password Found:#{rst} #{ylw}#{result}#{rst}" : "#{red}Password Not Found!#{rst}"
-   
-
-   when 3
-    result = brute_sha256(wordlist, hash)
-   puts result ?
-   "#{grn}Password Found:#{rst} #{ylw}#{result}#{rst}" : "#{red}Password Not Found!#{rst}"
-   
-
-   when 4
-    result = brute_sha384(wordlist, hash)
-   puts result ?
-   "#{grn}Password Found:#{rst} #{ylw} #{result}#{rst}" : "#{red}Password Not Found!#{rst}"
+       pass_crack()
 
    else
-    result = brute_sha512(wordlist, hash)
-   puts result ?
-   "#{grn}Password Found:#{rst} #{ylw}#{result}#{rst}" : "#{red}Password Not Found!#{rst}"
+       puts "\n#{red}Error: Invalid Input!#{rst}"
    end
    end
-  rescue Interrupt
-      puts "\n#{red}Terminated!#{rst}"
-end
+
+rescue Interrupt
+    puts "\n#{red}Terminated By User!#{rst}"
+    end
