1fa19bcc00f7dfca8be04835b23604e6b8e450bd
TAUREAN312 <jbotchwayowusu@gmail.com>
2025-02-19 23:20:37 +0000

    Update PassCrax.rb

--- PassCrax.rb
+++ PassCrax.rb
@@ -1,13 +1,14 @@
 #!/usr/bin/env ruby
 require './wordlist_cracker'
 require './pass_analyzer'
+require './salt_crack'
 begin
-  while true
    grn = "\e[32m"
    blu = "\e[34m"
    ylw = "\e[33m"
    red = "\e[31m"
    rst = "\e[0m"
+   loop do
 
    puts "\n#{blu}SElECT ONE\n#{rst}#{grn} [1]Password Analyzer\n [2]Password Cracker#{rst}"
    choose = gets.chomp.to_i
@@ -16,8 +17,20 @@ begin
        pass_analyze()
 
    when 2
-       pass_crack()
+       puts "\n#{blu}SELECT ONE\n#{rst}#{grn} [1]Salted Password Cracker\n [2]Unsalted Password Cracker#{rst}"
+       slct = gets.chomp.to_i
 
+       case slct
+       when 1
+           salt_crack()
+
+       when 2
+           pass_crack()
+
+       else
+           puts "\n#{red}Error: Invalid Input!#{rst}"
+   end
+   
    else
        puts "\n#{red}Error: Invalid Input!#{rst}"
    end
@@ -25,4 +38,4 @@ begin
 
 rescue Interrupt
     puts "\n#{red}Terminated By User!#{rst}"
-    end
+end
