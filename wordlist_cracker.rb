4b7e7362e6ee1efd417b55d90f12ef2b0d80b28d
TAUREAN312 <jbotchwayowusu@gmail.com>
2025-02-19 23:19:17 +0000

    Update wordlist_cracker.rb

--- wordlist_cracker.rb
+++ wordlist_cracker.rb
@@ -1,13 +1,12 @@
 #!/usr/bin/env ruby
 require 'digest'
-
 def pass_crack()
     grn = "\e[32m"
     blu = "\e[34m"
     ylw = "\e[33m"
     red = "\e[31m"
     rst = "\e[0m"
-    
+ begin
    puts "\n#{blu}Enter Hash String:#{rst}"
     hash = gets.chomp
     puts "\n#{blu}Enter Wordlist Filename:#{rst}"
@@ -16,7 +15,7 @@ def pass_crack()
    puts "\n#{blu}SELECT HASH FUNCTIONS\n#{rst} #{grn}[1] MD5\n [2] SHA-1\n [3] SHA-256\n [4] SHA-384\n [5] SHA-512\n#{rst}"
    input = gets.chomp.to_i
   def brute_md5(wordlist, hash)
-       File.readlines(wordlist).each do |word|
+       File.foreach(wordlist) do |word|
            word.chomp!
            return word if
            Digest::MD5.hexdigest(word) == hash
@@ -25,7 +24,7 @@ def pass_crack()
    end
 
    def brute_sha256(wordlist, hash)
-       File.readlines(wordlist).each do |word|
+       File.foreach(wordlist) do |word|
            word.chomp!
            return word if
            Digest::SHA256.hexdigest(word) == hash
@@ -34,7 +33,7 @@ def pass_crack()
    end
 
    def brute_sha1(wordlist, hash)
-       File.readlines(wordlist).each do |word|
+       File.foreach(wordlist) do |word|
            word.chomp!
            return word if
            Digest::SHA1.hexdigest(word) == hash
@@ -43,7 +42,7 @@ def pass_crack()
    end
 
    def brute_sha2(wordlist, hash)
-       File.readlines(wordlist).each do |word|
+       File.foreach(wordlist) do |word|
            word.chomp!
            return word if
            Digest::SHA2.hexdigest(word) == hash
@@ -52,7 +51,7 @@ def pass_crack()
    end
 
    def brute_sha384(wordlist, hash)
-       File.readlines(wordlist).each do |word|
+       File.foreach(wordlist) do |word|
            word.chomp!
            return word if
            Digest::SHA384.hexdigest(word) == hash
@@ -61,7 +60,7 @@ def pass_crack()
    end
 
    def brute_sha512(wordlist, hash)
-       File.readlines(wordlist).each do |word|
+       File.foreach(wordlist) do |word|
            word.chomp!
            return word if
            Digest::SHA512.hexdigest(word) == hash
@@ -96,4 +95,8 @@ def pass_crack()
    puts result ?
    "#{grn}Password Found:#{rst} #{ylw}#{result}#{rst}" : "#{red}Password Not Found!#{rst}"
    end
+end
+rescue Errno::ENOENT
+       puts "#{red}Error: Wordlist file not found!#{rst}"
+       exit
 end
