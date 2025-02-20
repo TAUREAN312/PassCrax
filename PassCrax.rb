51d1baa1b4325a732d9e7df9ba952327f722936d
TAUREAN312 <jbotchwayowusu@gmail.com>
2025-02-20 22:39:51 +0000

    Update PassCrax.rb

--- PassCrax.rb
+++ PassCrax.rb
@@ -10,15 +10,11 @@ begin
    rst = "\e[0m"
    loop do
 
-   puts "\n#{blu}SElECT ONE\n#{rst}#{grn} [1]Password Analyzer\n [2]Password Cracker#{rst}"
-   choose = gets.chomp.to_i
-   case choose
-   when 1
-       pass_analyze()
+   pass_analyze()
 
-   when 2
-       puts "\n#{blu}SELECT ONE\n#{rst}#{grn} [1]Salted Password Cracker\n [2]Unsalted Password Cracker#{rst}"
-       slct = gets.chomp.to_i
+  
+   puts "\n#{blu}SELECT ONE\n#{rst}#{grn} [1]Salted Password Cracker\n [2]Pure Password Cracker#{rst}"
+   slct = gets.chomp.to_i
 
        case slct
        when 1
@@ -29,10 +25,6 @@ begin
 
        else
            puts "\n#{red}Error: Invalid Input!#{rst}"
-   end
-   
-   else
-       puts "\n#{red}Error: Invalid Input!#{rst}"
    end
    end
 
