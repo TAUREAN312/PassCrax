95d4563a049304817622b73dac5cf1d99c3b5b58
TAUREAN312 <jbotchwayowusu@gmail.com>
2025-02-20 22:35:12 +0000

    Update salt_crack.rb

--- salt_crack.rb
+++ salt_crack.rb
@@ -1,17 +1,27 @@
 #!/usr/bin/env ruby
 
 require 'digest'
-#This script is meant to
-#Salt and hash words in a file
-#Compare them to the hashstring for answer
 
-def brute_all(wordlist,  hash, hashfunc)
-       File.foreach(wordlist) do |word|
+def brute_all(hash, hashfunc)
+    ylw = "\e[33m"
+    red = "\e[31m"
+    rst = "\e[0m"
+    wordlist_dir = "Wordlist/"
+    wordlist_files = Dir.glob("#{wordlist_dir}*.txt")
+    if wordlist_files.empty?
+        puts "\n#{red}Error: No Files Found In #{wordlist_dir}#{rst}"
+    return
+   end
+    (wordlist_files).each do |file|
+        puts "\n#{ylw}Scanning File: #{file}...#{rst}"
+        
+       File.foreach(file) do |word|
            word.chomp!
            return word if
            hashfunc.call(word) == hash
        end
        nil
+   end
    end
   
   #main
@@ -26,8 +36,6 @@ begin
    salt = gets.chomp
    puts "\n#{blu}Enter Salted Hash String:#{rst}"
     hash = gets.chomp
-    puts "\n#{blu}Enter Wordlist Filename:#{rst}"
-    wordlist = gets.chomp
    puts "\n#{blu}SELECT HASH FUNCTIONS\n#{rst} #{grn}[1] MD5\n [2] SHA-1\n [3] SHA-256\n [4] SHA-384\n [5] SHA-512\n#{rst}"
    input = gets.chomp.to_i
 
@@ -59,12 +67,9 @@ begin
    puts "\n#{red}Error: Invalid Input!#{rst}"
    end
    
-       result = brute_all(wordlist, hash, hashfunc)
+       result = brute_all(hash, hashfunc)
    puts result ?
-   "#{grn}Password Found:#{rst} #{ylw} #{result} #{rst}" : "#{red}Password Not Found!#{rst}"
-
-rescue Errno::ENOENT
-       puts "#{red}Error: Wordlist file not found!#{rst}"
-       exit
+   "\n#{grn}Password Found:#{rst} #{ylw} #{result} #{rst}" : "\n#{red}Password Not Found!#{rst}"
+   
      end
 end
