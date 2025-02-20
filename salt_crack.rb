aa32cafce68d2bffe7c935b2b653b2355c70250e
TAUREAN312 <jbotchwayowusu@gmail.com>
2025-02-20 08:30:33 +0000

    Update salt_crack.rb

--- salt_crack.rb
+++ salt_crack.rb
@@ -5,6 +5,16 @@ require 'digest'
 #Salt and hash words in a file
 #Compare them to the hashstring for answer
 
+def brute_all(wordlist,  hash, hashfunc)
+       File.foreach(wordlist) do |word|
+           word.chomp!
+           return word if
+           hashfunc.call(word) == hash
+       end
+       nil
+   end
+  
+  #main
 def salt_crack()
     grn = "\e[32m"
     blu = "\e[34m"
@@ -20,80 +30,41 @@ begin
     wordlist = gets.chomp
    puts "\n#{blu}SELECT HASH FUNCTIONS\n#{rst} #{grn}[1] MD5\n [2] SHA-1\n [3] SHA-256\n [4] SHA-384\n [5] SHA-512\n#{rst}"
    input = gets.chomp.to_i
-  def brute_md5(wordlist, salt, hash)
-       File.foreach(wordlist) do |word|
-           word.chomp!
-           return word if
-           Digest::MD5.hexdigest(salt + word) == hash
-       end
-       nil
-   end
 
-   def brute_sha256(wordlist, salt,  hash)
-       File.foreach(wordlist) do |word|
-           word.chomp!
-           return word if
-           Digest::SHA256.hexdigest(salt + word) == hash
-       end
-       nil
-   end
-
-   def brute_sha1(wordlist, salt, hash)
-       File.foreach(wordlist) do |word|
-           word.chomp!
-           return word if
-           Digest::SHA1.hexdigest(salt + word) == hash
-       end
-       nil
-   end
-
-   def brute_sha384(wordlist, salt, hash)
-       File.foreach(wordlist) do |word|
-           word.chomp!
-           return word if
-           Digest::SHA384.hexdigest(salt + word) == hash
-       end
-       nil
-   end
-
-   def brute_sha512(wordlist, salt, hash)
-       File.foreach(wordlist) do |word|
-           word.chomp!
-           return word if
-           Digest::SHA512.hexdigest(salt + word) == hash
-       end
-       nil
-   end
-   case input
+   hashfunc = case input
    when 1
-    result = brute_md5(wordlist, salt, hash)
-   puts result ?
-   "#{grn}Password Found:#{rst} #{ylw} #{result} #{rst}" : "#{red}Password Not Found!#{rst}"
+   lambda { |word|
+   Digest::MD5.hexdigest(salt + word) }
+
 
    when 2
-    result = brute_sha1(wordlist, salt, hash)
-   puts result ?
-   "#{grn}Password Found:#{rst} #{ylw}#{result}#{rst}" : "#{red}Password Not Found!#{rst}"
+   lambda { |word|
+   Digest::SHA1.hexdigest(salt + word) }
    
 
    when 3
-    result = brute_sha256(wordlist, salt, hash)
-   puts result ?
-   "#{grn}Password Found:#{rst} #{ylw}#{result}#{rst}" : "#{red}Password Not Found!#{rst}"
+   lambda { |word|
+   Digest::SHA256.hexdigest(salt + word) }
    
 
    when 4
-    result = brute_sha384(wordlist, salt, hash)
-   puts result ?
-   "#{grn}Password Found:#{rst} #{ylw} #{result}#{rst}" : "#{red}Password Not Found!#{rst}"
+   lambda { |word|
+   Digest::SHA384.hexdigest(salt + word) }
 
+   when 5
+   lambda { |word|
+   Digest::SHA512.hexdigest(salt + word) }
+   
    else
-    result = brute_sha512(wordlist, salt, hash)
-   puts result ?
-   "#{grn}Password Found:#{rst} #{ylw}#{result}#{rst}" : "#{red}Password Not Found!#{rst}"
+   puts "\n#{red}Error: Invalid Input!#{rst}"
    end
-end
+   
+       result = brute_all(wordlist, hash, hashfunc)
+   puts result ?
+   "#{grn}Password Found:#{rst} #{ylw} #{result} #{rst}" : "#{red}Password Not Found!#{rst}"
+
 rescue Errno::ENOENT
        puts "#{red}Error: Wordlist file not found!#{rst}"
        exit
+     end
 end
